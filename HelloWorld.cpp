e310e48e38d77cf5db588aca4828f4f8e4fd196a
Ethan Lockwood <lockwood.ethan@yahoo.com>
2025-09-23 16:25:35 -0400

    favorite number program

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,6 @@
 // How do you write C++? Let's find out!
 #include <iostream>
 
-// You can define a namespace so that you don't have to write std::cout or std::endl every time
 using namespace std;
 
 int main() {
