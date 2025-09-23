f07f8572fdbb907af6934294d3d6d62fa93a9a5f
Ethan Lockwood <lockwood.ethan@yahoo.com>
2025-09-23 16:35:53 -0400

    printing variable to cout on same line as string

--- FavoriteNumber.cpp
+++ FavoriteNumber.cpp
@@ -4,6 +4,6 @@ int main() {
 	int favorite_number;
 	std::cout << "Enter your favorite number between 1 and 100: ";
 	std::cin >> favorite_number;
-	std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+	std::cout << "Amazing!! That's my favorite number too! " << favorite_number << std::endl;
 	return 0;
 }
